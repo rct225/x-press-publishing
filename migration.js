1f34a56f128a7e2dca753a52ac39fbde43a59c76
Robert Tuck <robtuck@gmail.com>
2017-10-24 13:00:33 -0400

    fixed bug in migration.js -- excess comma

--- migration.js
+++ migration.js
@@ -3,19 +3,19 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 db.serialize(() => {
   //db.run(`DROP TABLE IF EXISTS Artist`);
-  db.run(`CREATE TABLE IF NOT EXISTS Artist(
+  db.run(`CREATE TABLE IF NOT EXISTS Artist (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     date_of_birth TEXT NOT NULL,
     biography TEXT NOT NULL,
-    is_currently_employed INTEGER DEFAULT 1
+    is_currently_employed INTEGER NOT NULL DEFAULT 1
   );`);
-  db.run(`CREATE TABLE IF NOT EXISTS Series(
+  db.run(`CREATE TABLE IF NOT EXISTS Series (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    description TEXT NOT NULL,
+    description TEXT NOT NULL
   );`);
-  db.run(`CREATE TABLE IF NOT EXISTS Issue(
+  db.run(`CREATE TABLE IF NOT EXISTS Issue (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     issue_number INTEGER NOT NULL,
