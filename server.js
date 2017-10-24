906d80d10f4d1c22f0ca14ef592ffda1449d90c7
Robert Tuck <robtuck@gmail.com>
2017-10-24 13:14:20 -0400

    added server directory to hold server components.  Added server/api.js to set up routes for resources.  Removed db stuff from server.js since it isn't needed there.  setup top-level route in server.js

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ module.exports = app;
 
 const PORT = process.env.PORT || 4001;
 
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT);
+});
