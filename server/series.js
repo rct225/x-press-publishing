b34c8a98c2809172b011f79e69d4ed7b04769b0a
Robert Tuck <robtuck@gmail.com>
2017-10-25 08:39:51 -0400

    Added GET and POST for /series

--- server/series.js
+++ server/series.js
@@ -3,3 +3,39 @@ const express = require('express');
 const seriesRouter = express.Router();
 
 module.exports = seriesRouter;
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+seriesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Series', (err, rows) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({series: rows});
+    }
+  });
+});
+
+seriesRouter.post('/', (req, res, next) => {
+  const seriesName = req.body.series.name;
+  const seriesDescription = req.body.series.description;
+  if (!seriesName || !seriesDescription) {
+    res.status(400).send();
+  } else {
+    db.run('INSERT INTO Series (name, description) ' +
+    'values($name, $description)', {
+      $name: seriesName,
+      $description: seriesDescription
+    }, function(err) {
+      if (err) {
+        next(err);
+      } else {
+        db.get(`SELECT * FROM Series WHERE id = ${this.lastID}`,
+          (error, row) => {
+            res.status(201).json({series: row});
+          });
+      }
+    });
+  }
+});
